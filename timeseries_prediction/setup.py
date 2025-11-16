8720f748011d03a53ebeb94648f99ce845edcea2
ajsinha <ajsinha@gmail.com>
2025-11-16 18:55:03 -0500

    redesigned and re done using factory pattern

--- timeseries_prediction/setup.py
+++ timeseries_prediction/setup.py
@@ -7,7 +7,7 @@ Ashutosh Sinha | Email: ajsinha@gmail.com
 
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("../README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
